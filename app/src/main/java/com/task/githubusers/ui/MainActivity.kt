4cdc9fcc828d8ed062bd8a72975546f2d68bd050
Muhammad Maqsood <mmaqsood@accelaero.com>
2022-04-20 21:11:23 +0400

    Add: UI containers added for user listing and details

--- app/src/main/java/com/task/githubusers/ui/MainActivity.kt
+++ app/src/main/java/com/task/githubusers/ui/MainActivity.kt
@@ -2,6 +2,11 @@ package com.task.githubusers.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.task.githubusers.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,5 +15,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        toolbar.setupWithNavController(getNavController())
+    }
+
+    private fun getNavController(): NavController {
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        return navHostFragment.navController
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
